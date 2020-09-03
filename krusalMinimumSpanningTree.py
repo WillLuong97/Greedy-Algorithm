15ff8db8020c93a726bb1765dffa147f62be8c81
Will Luong <tluong@stedwards.edu>
2020-09-03 14:55:06 -0500

    Create krusalMinimumSpanningTree.py

---
+++ krusalMinimumSpanningTree.py
@@ -0,0 +1,8 @@
+#Python program to solve the Kruskal Minimum Spanning Tree problem using the Greedy algorithm 
+
+def main(): 
+	print("TESTING KRUSKAL MST PROBLEM...")
+
+	print("END OF TESTING...")
+
+main()
