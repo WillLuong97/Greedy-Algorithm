9bd2f89678762d9ba6a1c452da0aa118ff534358
Will Luong <tluong@stedwards.edu>
2020-09-03 18:16:07 -0500

    Create wiggleSubsequent.py

---
+++ wiggleSubsequent.py
@@ -0,0 +1,14 @@
+Python3 implementation of the Leetcode problem 376, Wiggle Subsequences
+#The problem will mainly focus on the Greedy approach
+
+
+#main function to test and run the algorithm 
+def main():
+	print("TESTING WIGGLE SUBSEQUENCE...")
+
+	print("END OF PROGRAM...")
+main()
+
+
+
+
