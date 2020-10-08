479ba67bea13900d2d97f0bc89c1de45303a3a69
Will Luong <tluong@stedwards.edu>
2020-10-08 15:42:39 -0500

    Create removeDuplicateNumber.py
    Python3 implementation of the problem using stack + greedy approach.

---
+++ removeDuplicateNumber.py
@@ -0,0 +1,67 @@
+#Leetcode 316. Remove Duplicate Letters
+
+#Problem statement: 
+
+'''
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+ 
+
+Example 1:
+
+Input: s = "cdadabcc"
+Output: "adbc"
+Example 2:
+
+Input: s = "abcd"
+Output: "abcd"
+Example 3:
+
+Input: s = "ecbacba"
+Output: "eacb"
+Example 4:
+
+Input: s = "leetcode"
+Output: "letcod"
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lowercase English letters.
+'''
+def removeDuplicateLetters(s):
+    #base case: 
+    if not s: 
+        return ""
+
+    #create a stack to store our answer
+    stack = []
+    #append the first element into the stack, this way we have an element in the stack to deal with 
+    stack.append(s[0])
+    #loop through the original string
+    for i in range(len(s)):
+        if s[i] not in stack:
+            while stack and ord(stack[-1]) > ord(s[i]) and stack[-1] in s[i+1:]:
+                stack.pop()
+            stack.append(s[i])
+
+    return "".join(stack)
+
+
+
+#Driver code: 
+def main():
+    print("TESTING REMOVE DUPLICATE NUMBER...")
+    input01 = "cdadabcc"
+    input02 = "abcd"
+    input03 = "ecbacba"
+    input04 = "leetcode"
+    print(removeDuplicateLetters(input01))
+    print(removeDuplicateLetters(input02))
+    print(removeDuplicateLetters(input03))
+    print(removeDuplicateLetters(input04))
+    print("END OF TESTING")
+main()
